3a26cba171224221e650e7c51d2a57d524801ad3
cstsncv <cstsncv@sina.com>
2021-12-14 15:09:53 +0800

    sqlSession

---
+++ mybatis_01_HelloWorld/src/main/java/com/csts/mybatis/dao/EmployeeMapper.java
@@ -0,0 +1,7 @@
+package com.csts.mybatis.dao;
+
+import com.csts.mybatis.bean.Employee;
+
+public interface EmployeeMapper {
+    public Employee getEmployeeById(Integer id);
+}
--- mybatis_01_HelloWorld/src/test/java/com/csts/mybatis/bean/EmployeeTest.java
+++ mybatis_01_HelloWorld/src/test/java/com/csts/mybatis/bean/EmployeeTest.java
@@ -1,5 +1,6 @@
 package com.csts.mybatis.bean;
 
+import com.csts.mybatis.dao.EmployeeMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -13,18 +14,48 @@ import static org.junit.Assert.*;
 
 public class EmployeeTest {
 
-    @Test
-    public void EmployeeTest1() throws IOException {
+    public SqlSessionFactory getSqlSessionFactory() throws IOException {
         String resource = "com/csts/mybatis/conf/mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory =
                 new SqlSessionFactoryBuilder().build(inputStream);
+        return sqlSessionFactory;
+    }
+
+    // 1. 原版本
+    @Test
+    public void EmployeeTest1() throws IOException {
+        // 1. 获取sqlSessionFactory对象
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
+
+        // 2. 获取sqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3.
+        try {
+            Employee employee = sqlSession.selectOne("com.csts.mybatis.conf.EmployeeMapper.selectEmp", 1);
+            System.out.println(employee);
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+
+    // 2. 新版本, 接口式
+    @Test
+    public void test01() throws IOException {
+
+        SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
 
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
         try {
-            Employee employee = sqlSession.selectOne("com.csts.mybatis.conf.EmployeeMapper.selectEmp", 1);
-            System.out.println(employee);
+            // 获取接口实现类对象,  会为接口自动创建代理对象mapper, 代理对象去执行sql
+            EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+
+            Employee employeeById = mapper.getEmployeeById(1);
+
+            System.out.println(employeeById);
         } finally {
             sqlSession.close();
         }
